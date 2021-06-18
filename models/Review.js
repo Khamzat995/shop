1b10a368809dc4fa2cdb674c0365ef4b686178dd
jamboooooo <hisllam35@gmail.com>
2021-06-18 15:10:11 +0300

    added model Cart and Review, add controller cart, review and add routes for cart and review

--- models/Review.js
+++ models/Review.js
@@ -1,3 +1,20 @@
 const mongoose = require("mongoose");
-const ReviewSchema = mongoose.Schema({});
-module.exports = mongoose.model("Review", ReviewSchema);
+const reviewSchema = mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: Number,
+        required: true
+    },
+    userId: {
+        ref: 'User',
+        type: mongoose.Schema.Types.ObjectId
+    },
+    productId: {
+        ref: 'Product',
+        type: mongoose.Schema.Types.ObjectId
+    }
+});
+module.exports = mongoose.model("Review", reviewSchema);
